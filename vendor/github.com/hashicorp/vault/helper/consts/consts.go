2d97f58422e8d5b4002a8ef440422c1443ea169f
Rayvorn Patterson <rayvorn@gmail.com>
2019-04-14 19:53:07 -0400

    Configure Go modules

--- vendor/github.com/hashicorp/vault/helper/consts/consts.go
+++
@@ -1,9 +0,0 @@
-package consts
-
-const (
-	// ExpirationRestoreWorkerCount specifies the number of workers to use while
-	// restoring leases into the expiration manager
-	ExpirationRestoreWorkerCount = 64
-
-	VaultKVCLIClientHeader = "X-Vault-Kv-Client"
-)
